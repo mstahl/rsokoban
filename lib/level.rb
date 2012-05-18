354b6df7187def384af050514c62791d56844585
Max Thom Stahl <max@villainousindustri.es>
2012-05-18 01:33:14 -0500

    Mostly done.

--- lib/level.rb
+++ lib/level.rb
@@ -1,15 +1,67 @@
+require 'happymapper'
+
 module RSokoban
-  class Level
+  attr_accessor :levels
+  
+  class LevelData
     include HappyMapper
     
+    tag 'Level'
     element :id, String
     element :copyright, String
     element :width, Integer
     element :height, Integer
-    has_many :l, String
+    has_many :lines, String, :tag => 'L'
+  end
+  
+  class Level
+    attr_accessor :walls
+    attr_accessor :boxes
+    attr_accessor :player
+    attr_accessor :goals
+    
+    def initialize(data)
+      @walls  = {}
+      @boxes  = {}
+      @player = {}
+      @goals  = {}
+      
+      data.lines.each_with_index do |line, row|
+        line.split(//).each_with_index do |char, col|
+          case char
+          when '#'
+            @walls[[row, col]] = true
+          when '$'
+            @boxes[[row, col]] = true
+          when '.'
+            @goals[[row, col]] = true
+          when '@'
+            @player = {row: row, col: col}
+          end
+        end
+      end
+    end
+    
+    def draw(window)
+      @walls.keys.each do |k|
+        window.move k[0], k[1]
+        window.addstr '#'
+      end
+
+      @goals.keys.each do |k|
+        window.move k[0], k[1]
+        window.addstr '.'
+      end
+    end
+    
+    def self.parse(string)
+      LevelData.parse(string).map do |level|
+        Level.new level
+      end
+    end
     
     def self.load(filename)
-      Level.parse(File.open(filename).read).each{|elt| ap elt}
+      self.parse(File.open(filename).read)
     end
   end
 end
